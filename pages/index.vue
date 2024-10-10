d926d49d444602ea1f4e1ec07f5fdd89476bddf5
hrynevychroman <hrynevych.romann@gmail.com>
2024-10-10 09:01:34 +0300

    feat(global): add interesting links to `index` page

--- pages/index.vue
+++ pages/index.vue
@@ -7,5 +7,29 @@
       Click on the navigation links above to see different form validation
       examples.
     </p>
+
+    <h2 class="text-xl font-semibold mb-4 mt-8">
+      Interesting Links
+    </h2>
+    <ul class="flex flex-col gap-2">
+      <li>
+        <a class="hover:underline" href="https://vee-validate.logaretm.com/v4/guide/composition-api/nested-objects-and-arrays/">Nested Objects and Arrays</a>
+      </li>
+      <li>
+        <a class="hover:underline" href="https://vee-validate.logaretm.com/v4/examples/multistep-form-wizard/">Multistep Form Wizard</a>
+      </li>
+      <li>
+        <a class="hover:underline" href="https://vee-validate.logaretm.com/v4/guide/composition-api/helpers/">Helpers</a>
+      </li>
+      <li>
+        <a class="hover:underline" href="https://vee-validate.logaretm.com/v4/guide/composition-api/caveats/">Best Practices</a>
+      </li>
+      <li>
+        <a class="hover:underline" href="https://vee-validate.logaretm.com/v4/examples/ui-libraries/">UI Libraries</a>
+      </li>
+      <li>
+        <a class="hover:underline" href="https://github.com/aiji42/zod-i18n">Zod i18n</a>
+      </li>
+    </ul>
   </div>
 </template>
